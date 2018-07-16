dc7722b9aef07f6f1475757d4b8ca1e048ca3bbb
Gert Wohlgemuth <berlinguyinca@gmail.com>
2018-07-16 12:30:21 -0700

    minor fixes

--- freqtrade/optimize/backtesting.py
+++ freqtrade/optimize/backtesting.py
@@ -96,7 +96,7 @@ class Backtesting(object):
 
         self.use_backslap = True             # Enable backslap - if false Orginal code is executed.
         self.debug = False                   # Main debug enable, very print heavy, enable 2 loops recommended
-        self.debug_timing = True            # Stages within Backslap
+        self.debug_timing = False            # Stages within Backslap
         self.debug_2loops = False            # Limit each pair to two loops, useful when debugging
         self.debug_vector = False            # Debug vector calcs
         self.debug_timing_main_loop = False  # print overall timing per pair - works in Backtest and Backslap
